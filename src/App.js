98048dfc18ece9ce36f8a38b7ab44f70310cfd6d
FraanMoore <fraanmoore@gmail.com>
2023-02-10 22:38:28 -0300

    to do list

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from 'fontawesome';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+function App() {
+  const listo = <FontAwesomeIcon icon="fa-thin fa-xmark" />
+  const [inputValue, setInputValue] = useState("")
+  const [lista, setLista] = useState([])
+  const [titulo, setTitulo] = useState("")
+  return (
+    
+    <div className='container ' id='containertask'>
+
+      <div className="input-group">
+        <input id="titulo" type="text" aria-label="First name" className="form-control" placeholder='Titulo de mis tareas' 
+       />
+        
+        
+      </div>
+
+      <div  className="input-group mb-3">
+        <input id='box' type="text" className="form-control" placeholder="¿Qué harás hoy?" aria-label="Recipient's username" aria-describedby="button-addon2" 
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setLista(lista.concat(inputValue));
+            setInputValue("");
+          }
+        }}/>
+</div>
+        <ul>
+        {lista.map((item, index) => 
+          <li>
+            <p>{item}{""}
+            <button className="btn btn-outline-success"  onClick={() => setLista(lista.filter((t, currentIndex) => index !== currentIndex))}> X </button>
+            </p></li> )}
+        </ul>       
+      <div> <p className='task'>{lista.length} tareas</p></div>
+   
+   
+    </div>
+  );
+}
+
+export default App;
